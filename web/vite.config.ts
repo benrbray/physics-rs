0d55b17f2dc450286236352ff908592d519d0af8
Benjamin R. Bray <benrbray@gmail.com>
2025-06-02 14:57:45 +0900

    bundle wasm-physics with package

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -30,8 +30,8 @@ export default defineConfig({
       // dependencies will be installed by the consumer,
       // so tell rollup not to bundle them with the package
       external: [
-        "wasm-physics/wasm_physics",
-        "wasm-physics/wasm_physics_bg.wasm",
+        // "wasm-physics/wasm_physics",
+        // "wasm-physics/wasm_physics_bg.wasm",
         ...Object.keys(pkg["dependencies"]     || {}),
         ...Object.keys(pkg["peerDependencies"] || {}),
         ...Object.keys(pkg["devDependencies"]  || {}),
